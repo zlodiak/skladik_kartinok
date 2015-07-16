985ad08b6e59b6e07fd7321be1a1e38bbef968ab
zlodiak <prozaik81-2@yandex.ru>
2015-07-16 12:59:04 +0300

    before add field to reg form

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,4 +1,10 @@
 class PersonsController < ApplicationController
   def profile
   end
+
+  private 
+
+    def person_params
+      params.require(:person).permit(:name, :email, :password, :password_confirmation)
+    end
 end
