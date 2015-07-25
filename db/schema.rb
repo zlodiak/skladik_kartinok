54cb2d2fde9bc8ce22b6a0e3a2766365b185ba91
zlodiak <prozaik81-2@yandex.ru>
2015-07-25 11:10:13 +0300

    create table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722122241) do
+ActiveRecord::Schema.define(version: 20150725074312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.boolean  "closed"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string "title"
