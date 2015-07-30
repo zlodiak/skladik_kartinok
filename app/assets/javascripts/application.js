ab8d4a7aa3e55ab91fe233daed39c4d4d1cbf4c3
zlodiak <prozaik81-2@yandex.ru>
2015-07-30 20:02:01 +0300

    ajax del and create refactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require_tree .
 
 $( document ).ready(function() {
+    // main menu some punkts handlers
     $('#about').on('click', function(e){
       e.preventDefault();
       $('#modalAbout').modal();
@@ -65,31 +66,40 @@ $( document ).ready(function() {
         type: 'POST',
         dataType: "JSON", 
         data: $('#new_album').serialize(),
-        success: function(result){
+        success: function(album){
           handleModal('Альбом создан', '', '00ff2a', 2000);
           albumTitle.val('');
           albumDescription.val('');
-          $('#albumsList').prepend('<article class="col-xs-12 col-sm-6 col-md-4" data-current-user="' + currentUserId + '" data-album-id="' + result.id + '"> \
-            <h3 class="title">' + result.title + '</h3> \
-            <p class="body">' + result.description + '</p> \
-            <p class="details"> \
-              <a class="btn btn-default" href="/users/' + currentUserId + '/albums/' + result.id + '">Подробнее...</a> \
-              <span class="glyphicon glyphicon-remove pull-right destroy_album"></span> \
-            </p> \
-          </article>');
+          $('#albumsList').prepend(handleConstructAlbumTeaser(currentUserId, album));
         },
         error: function(xhr, ajaxOptions, thrownError){
           var errorText = '';
 
           $.each(JSON.parse(xhr.responseText), function(key, val){
-            errorText += (key + 1) + '.' + val + '<br /><br />'
+            errorText += (key+1) + '.' + val + '<br /><br />'
           })
           handleModal('Альбом не создан', errorText, 'f00', 10000);
         }        
       })
     });  
 
-    // handle modal window
+    // handle construct album teaser
+    function handleConstructAlbumTeaser(currentUserId, album){
+      var albumTeaser;
+
+      albumTeaser = '<article class="col-xs-12 col-sm-6 col-md-4" data-current-user="' + currentUserId + '" data-album-id="' + album.id + '"> \
+            <h3 class="title">' + album.title + '</h3> \
+            <p class="body">' + album.description + '</p> \
+            <p class="details"> \
+              <a class="btn btn-default" href="/users/' + currentUserId + '/albums/' + album.id + '">Подробнее...</a> \
+              <span class="glyphicon glyphicon-remove pull-right destroy_album"></span> \
+            </p> \
+          </article>';
+
+      return albumTeaser;
+    }
+
+    // handle modal window. for all ajax-requests
     function handleModal(title, body, colorHex, timeout){
       $('#titleModalInfo').html(title).css({'color': '#' + colorHex});
       $('#bodyModalInfo').html(body);
@@ -103,8 +113,6 @@ $( document ).ready(function() {
         $('#bodyModalInfo').empty();
       };
     }
-
-
 });
 
 
