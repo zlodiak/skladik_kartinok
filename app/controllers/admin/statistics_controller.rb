9a9428ed982a39c800ca93a0a39043e0de77d8a7
zlodiak <prozaik81-2@yandex.ru>
2015-08-30 11:34:12 +0300

    stat complete

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -9,5 +9,15 @@ class Admin::StatisticsController < ApplicationController
     @managers_quantity = User.where(status_id: 1).count()
     @users_quantity = User.where(status_id: 0).count()
     @accounts_with_avatar_quantity = User.where.not(avatar_file_name: nil).count()
+
+    @images_quantity = Image.where(is_delete: nil).count()
+    @images_in_poll_quantity = Image.where.not(poll_id: nil).count()
+
+    @polls_quantity = Poll.all.count()
+    @polls_closed_quantity = Poll.where(status_poll_id: 1).count()
+    @polls_opened_quantity = Poll.where(status_poll_id: 0).count()
+
+    @albums_quantity = Album.all.count()
+    @average_images_in_album_quantity = @images_quantity / Album.all.count()
   end
 end
