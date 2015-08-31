58cc0ac1dccd88e333363943abd15a4fb41e58f7
zlodiak <prozaik81-2@yandex.ru>
2015-08-31 21:52:40 +0300

    user ok

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe User do
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it "is invalid without name" do
+    expect(FactoryGirl.build(:user, name: nil)).not_to be_valid
+  end  
+  
+  it "is invalid without email" do
+    expect(FactoryGirl.build(:user, email: nil)).not_to be_valid
+  end     
+
   describe 'associations' do
    it "belongs_to status" do
       should belong_to(:status)
@@ -17,5 +29,13 @@ describe User do
    it "has_many polls" do
       should have_many(:polls)
    end           
+  end  
+
+  describe 'avatar' do
+    it "is valid when avatar is empty" do
+      expect(FactoryGirl.build(:user, 
+        avatar_file_name: nil
+      )).to be_valid
+    end         
   end  
 end  
