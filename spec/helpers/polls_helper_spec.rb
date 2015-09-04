2e2522b8869d201096b98873f932e4f7bc2f90d1
zlodiak <prozaik81-2@yandex.ru>
2015-09-04 20:04:03 +0300

    albums routing ok

--- spec/helpers/polls_helper_spec.rb
+++ spec/helpers/polls_helper_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
+require 'helpers/application_helper_spec'
+
+RSpec.configure do |c|
+  c.include Helpers
+end
+
 
 describe PollsHelper do
   describe "string concat" do
@@ -10,7 +16,13 @@ describe PollsHelper do
 
     it "get_summary_votes_for_image return nil for fail images" do
       expect(ImageLike.where(image_id: 1).count()).to eq(0)
-    end    
+    end  
+
+    it "return true for not signed user" do
+      val = user_vote_image_check(1)
+
+      expect(val).to be_false
+    end        
   end
 end
 
---
+++ spec/routing/albums_routing_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+RSpec.describe AlbumsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/users/1/albums").to route_to(
+        "controller" => "albums", 
+        "action" => "index", 
+        "user_id" => "1"
+      )
+    end
+
+
+    it "routes to #new" do
+      expect(:get => "/users/1/albums/new").to route_to(
+        "controller" => "albums", 
+        "action" => "new", 
+        "user_id" => "1"
+      )
+    end
+
+    it "routes to #show" do
+      expect(:get => "/users/1/albums/1").to route_to(
+        "controller" => "albums", 
+        "action" => "show", 
+        "user_id" => "1",
+        "id" => "1"
+      )
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/users/1/albums/1/edit").to route_to(
+        "controller" => "albums", 
+        "action" => "edit", 
+        "user_id" => "1",
+        "id" => "1"
+      )
+    end
+
+    it "routes to #create" do
+      expect(:post => "/users/1/albums").to route_to(
+        "controller" => "albums", 
+        "action" => "create", 
+        "user_id" => "1",
+      )
+    end
+
+    it "routes to #update via PUT" do
+      expect(:put => "/users/1/albums/1").to route_to(
+        "controller" => "albums", 
+        "action" => "update", 
+        "user_id" => "1",
+        "id" => "1"
+        )
+    end
+
+    it "routes to #update via PATCH" do
+      expect(:patch => "/users/1/albums/1").to route_to(
+        "controller" => "albums", 
+        "action" => "update", 
+        "user_id" => "1",
+        "id" => "1"
+      )
+    end    
+
+    it "routes to #destroy" do
+      expect(:delete => "/users/1/albums/1").to route_to(
+        "controller" => "albums", 
+        "action" => "destroy", 
+        "user_id" => "1",
+        "id" => "1"
+      )
+    end
+  end
+end
+
